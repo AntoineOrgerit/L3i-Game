99e32aa3604a61fd06c1c9cb597308d6020cb77e
lisa.fougeron <lisa.fougeron@etudiant.univ-lr.fr>
2019-09-23 14:06:32 +0200

    Fixing - scan info

--- QuizzApp/www/js/my-js.js
+++ QuizzApp/www/js/my-js.js
@@ -1,16 +1,15 @@
-
 /** ----- GENERAL SETTINGS -----  **/
 
 /**
  * Fixing proxy issue
  */
-(function() {
+(function () {
     var xhr = {};
     xhr.open = XMLHttpRequest.prototype.open;
 
-    XMLHttpRequest.prototype.open = function(method, url) {
+    XMLHttpRequest.prototype.open = function (method, url) {
         console.log(url);
-        if(url.indexOf('/proxy/') == 0){
+        if (url.indexOf('/proxy/') == 0) {
             url = window.decodeURIComponent(url.substr(7));
         }
         xhr.open.apply(this, arguments);
@@ -26,9 +25,6 @@ $(document).on("mobileinit", function () {
 });
 
 
-
-
-
 /** ----- LOADER SETTINGS -----  **/
 
 /**
@@ -40,7 +36,7 @@ $(window).load(hideLoader);
  * Shows the loader.
  */
 function showLoader() {
-	document.getElementById("content").style.display = "none";
+    document.getElementById("content").style.display = "none";
     if (document.getElementById("spinner") != null) document.getElementById("spinner").style.display = "block";
 }
 
@@ -48,12 +44,9 @@ function showLoader() {
  * Hides the loader.
  */
 function hideLoader() {
-	document.getElementById("content").style.display = "block";
+    document.getElementById("content").style.display = "block";
     if (document.getElementById("spinner") != null) document.getElementById("spinner").style.display = "none";
 }
-
-
-
 
 
 /** ----- PAGE CONTENT LOADING FROM SERVER ----- **/
@@ -61,79 +54,88 @@ function hideLoader() {
 /**
  * View specific treatment redirection.
  */
-$(document).on("pagebeforechange", function(e, data) {
-	switch(data.toPage[0].id) {
-	case "categories-view":
-		loadCategoriesView();
-		break;
-	case "game-view":
-		loadQuestionView(data);
-		break;
-	default:
-	}
+$(document).on("pagebeforechange", function (e, data) {
+    switch (data.toPage[0].id) {
+        case "categories-view":
+            loadCategoriesView();
+            break;
+        case "game-view":
+            loadQuestionView(data);
+            break;
+        default:
+    }
 });
 
 /**
  * Categories loading.
  */
 function loadCategoriesView() {
-	showLoader();
-	var url = appConfig['Server-URL'] + "getCategories.php";
-	$.getJSON(url, function(result) {
-		$("#categories-list").empty();
-		$.each(result, function(i, field) {
-			$("#categories-list").append("<a class='ui-btn ui-shadow ui-corner-all' data-role='button' data-transition='none' data-category-id='" + field.id + "'>" + field.theme + "</a>");
-		});
-		$('#categories-list a').on('click', function(event) {
-			event.preventDefault();
-			showLoader();
-			$.mobile.changePage('#game-view', { category_id: $(this).data("category-id"), transition: "none" });
-		});
-		hideLoader();
-	}).error(function() {
-		hideLoader();
-		alert("Erreur lors de l'obtention des catégories de jeu.");
-	});
+    showLoader();
+    var url = appConfig['Server-URL'] + "getCategories.php";
+    $.getJSON(url, function (result) {
+        $("#categories-list").empty();
+        $.each(result, function (i, field) {
+            $("#categories-list").append("<a class='ui-btn ui-shadow ui-corner-all' data-role='button' data-transition='none' data-category-id='" + field.id + "'>" + field.theme + "</a>");
+        });
+        $('#categories-list a').on('click', function (event) {
+            event.preventDefault();
+            showLoader();
+            $.mobile.changePage('#game-view', {category_id: $(this).data("category-id"), transition: "none"});
+        });
+        hideLoader();
+    }).error(function () {
+        hideLoader();
+        alert("Erreur lors de l'obtention des catégories de jeu.");
+    });
 }
 
 /**
  * Question loading.
  */
 function loadQuestionView(data) {
-	var category_id = data.options.category_id;
-	var url = appConfig['Server-URL'] + "getQuestion.php";
-	$.post(url, {
-			category_id: category_id
-	}, function(result) {
-		var question_data = JSON.parse(result);
-		$("#question").text(question_data.intitule);
-		$('#scan-res-button').unbind().click(function() {
-			scanAnswer(question_data.answers);
+    var category_id = data.options.category_id;
+    var url = appConfig['Server-URL'] + "getQuestion.php";
+    $.post(url, {
+        category_id: category_id
+    }, function (result) {
+        var question_data = JSON.parse(result);
+        $("#question").text(question_data.intitule);
+		$('#scan-info-button').unbind().click(function () {
+			scanInfo();
 		});
-		hideLoader();
-	}).error(function(err) {
-		hideLoader();
-		alert("Erreur lors de l'obtention de la question.");
-	});
-	hideLoader();
+        $('#scan-res-button').unbind().click(function () {
+            scanAnswer(question_data.answers);
+        });
+        hideLoader();
+    }).error(function (err) {
+        hideLoader();
+        alert("Erreur lors de l'obtention de la question.");
+    });
+    hideLoader();
 }
 
 
-
-
-
 /** ----- SCAN INTERACTIONS ----- **/
+
+function scanInfo() {
+    cordova.plugins.barcodeScanner.scan(function (result) {
+            alert(result.text);
+        }, function (error) {
+            alert(JSON.stringify(error));
+        }
+    )
+}
 
 /**
  * Answer QRCODE scanning.
  */
 function scanAnswer(answers) {
-	cordova.plugins.barcodeScanner.scan(function (result) {
-		if(isAnAnswer(result.text, answers)) {
-			alert("Bonne réponse !");
-		} else {
-			alert("Mauvaise réponse...");
-		}
+    cordova.plugins.barcodeScanner.scan(function (result) {
+        if (isAnAnswer(result.text, answers)) {
+            alert("Bonne réponse !");
+        } else {
+            alert("Mauvaise réponse...");
+        }
     }, function (error) {
         alert(JSON.stringify(error));
     });
@@ -143,10 +145,10 @@ function scanAnswer(answers) {
  * Proposition verifying.
  */
 function isAnAnswer(proposition, answers) {
-	for(var i=0; i<answers.length; i++) {
-		if(answers[i].intitule == proposition) {
-			return true;
-		}
-	}
-	return false;
+    for (var i = 0; i < answers.length; i++) {
+        if (answers[i].intitule == proposition) {
+            return true;
+        }
+    }
+    return false;
 }
