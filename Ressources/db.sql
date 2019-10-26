18cee8ce6bead40b92657c1c44ac27e72787f681
Antoine Orgerit <orgerit.antoine@gmail.com>
2019-10-26 15:18:40 +0200

    Updating database file and getQuestion.php to include the 'image' column.

--- Ressources/db.sql
+++ Ressources/db.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: l3igame
 -- ------------------------------------------------------
--- Server version	5.7.26
+-- Server version	8.0.17
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -50,7 +50,7 @@ DROP TABLE IF EXISTS `indice`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `indice` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `intitule` text COLLATE utf8_bin NOT NULL,
+  `intitule` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `priorite` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
@@ -110,7 +110,7 @@ CREATE TABLE `question` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_categorie` int(11) NOT NULL,
   `id_niveau` int(11) NOT NULL,
-  `intitule` text NOT NULL,
+  `intitule` text,
   `image` text,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
@@ -118,7 +118,7 @@ CREATE TABLE `question` (
   KEY `id_niveau_fk_idx` (`id_niveau`),
   CONSTRAINT `id_categorie_fk` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id`),
   CONSTRAINT `id_niveau_fk` FOREIGN KEY (`id_niveau`) REFERENCES `niveau` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,7 +127,7 @@ CREATE TABLE `question` (
 
 LOCK TABLES `question` WRITE;
 /*!40000 ALTER TABLE `question` DISABLE KEYS */;
-INSERT INTO `question` VALUES (1,2,2,'Qui a participé à l\'écriture de l\'article : \" Analyse d’Images de Documents Anciens : une Approche Texture\" ?',NULL),(2,3,1,'Qui développe la librairie de manipulation des treillis ?',NULL),(3,1,2,'Qui sont les directeurs de thèse de Damien Mondou ?',NULL),(4,3,1,'Qui est impliqué dans les projets Art et Sciences ?',NULL),(5,2,3,'Qui a participé à l\'écriture de l\'article : \"Une approche ontologique pour la structuration de données spatio-temporelles de trajectoires : Application à l’étude des déplacements de mammifères marins\" ?',NULL),(6,3,2,'Qui travaille sur les scénarios interactifs et la scénarisation adaptive ?',NULL),(7,2,2,'Qui a écrit l\'article \"Reconnaissance de symboles à partir d’une signature structurelle flexible et d’un classifieur de type treillis de Galois.\" ?',NULL),(8,2,3,'Qui a écrit l\'article \"Système générique et omni-langage de navigation dans des bases de documents anciens basé sur de la recherche de mots par composition interactive de requêtes.\" ?',NULL),(9,1,3,'Qui ont été les directeurs de thèse de Jean-François Viaud ?',NULL),(10,3,1,'Qui a effectué une thèse sur le thème des treillis ?',NULL),(11,3,2,'Qui travaille à résoudre les conflits d\'interaction dans les systèmes multi-agents ?',NULL),(12,2,3,'Qui a écrit l\'article \"Genca : un modèle général de négociation de contrats.\" ?',NULL),(13,3,2,'Qui travaile sur l’intégration des traces d’exécution dans les mécanismes de pilotage des applications ?',NULL),(14,2,3,'Qui a écrit l\'article \"Aide à l’analyse des parcours d’apprentissage en IUT par reconnaissance de procédés et recommandations à base de traces\" ?',NULL),(15,3,3,'Qui travaille sur l\'adaptation dynamique des contenus, notamment par scénarisation ?',NULL),(16,2,3,'Qui a écrit l\'article \"Scénarisation et exécution adaptative\" ?',NULL),(17,3,2,'Qui travaille sur les problématiques et techniques liées à l\'indexation ?',NULL),(18,1,1,'Qui était le directeur de thèse de Mickael Coustaty ?',NULL),(19,1,1,'Qui était le directeur de thèse de Frédéric Bertrand ?',NULL),(20,1,1,'Quels chercheurs font partie de l’équipe de projet eBDthèque ?',NULL),(21,1,2,'Quels chercheurs font partie de l’équipe de projet SHADES ?',NULL),(22,1,2,'Quels chercheurs font partie de l’équipe de projet Chaire d’excellence ‘Gestion du conflit et de l’après-conflit’?',NULL),(23,2,1,'Qui a écrit l’article ‘Generation algorithm of a concept lattice with limited object access’ ?',NULL),(24,2,1,'Qui a écrit l’article ‘Vers une réduction du fossé sémantique dans le traitement des images de documents anciens à base d’ontologies : Application aux lettrines’ ?',NULL),(25,2,2,'Qui a écrit l’article ‘Real-time face tracking for attention aware adaptive games’ ?',NULL),(26,2,2,'Qui a écrit l’article ‘A new shape descriptor working in Discrete Cosine space : application to graphical symbols recognition’ ?',NULL),(27,3,2,'Qui travaille les extractions et les manipulations de connaissances ?',NULL),(28,3,3,'Qui travaille sur la modélisation d’expressions temporelles exprimant une périodicité ?',NULL),(29,3,3,'Qui travaille sur le pilotage de récits interactifs ?',NULL),(30,3,3,'Qui travaille sur la mobilité et l’intégration des données ?',NULL);
+INSERT INTO `question` VALUES (1,2,2,'Qui a participé à l\'écriture de l\'article : \" Analyse d’Images de Documents Anciens : une Approche Texture\" ?',NULL),(2,3,1,'Qui développe la librairie de manipulation des treillis ?',NULL),(3,1,2,'Qui sont les directeurs de thèse de Damien Mondou ?',NULL),(4,3,1,'Qui est impliqué dans les projets Art et Sciences ?',NULL),(5,2,3,'Qui a participé à l\'écriture de l\'article : \"Une approche ontologique pour la structuration de données spatio-temporelles de trajectoires : Application à l’étude des déplacements de mammifères marins\" ?',NULL),(6,3,2,'Qui travaille sur les scénarios interactifs et la scénarisation adaptive ?',NULL),(7,2,2,'Qui a écrit l\'article \"Reconnaissance de symboles à partir d’une signature structurelle flexible et d’un classifieur de type treillis de Galois.\" ?',NULL),(8,2,3,'Qui a écrit l\'article \"Système générique et omni-langage de navigation dans des bases de documents anciens basé sur de la recherche de mots par composition interactive de requêtes.\" ?',NULL),(9,1,3,'Qui ont été les directeurs de thèse de Jean-François Viaud ?',NULL),(10,3,1,'Qui a effectué une thèse sur le thème des treillis ?',NULL),(11,3,2,'Qui travaille à résoudre les conflits d\'interaction dans les systèmes multi-agents ?',NULL),(12,2,3,'Qui a écrit l\'article \"Genca : un modèle général de négociation de contrats.\" ?',NULL),(13,3,2,'Qui travaile sur l’intégration des traces d’exécution dans les mécanismes de pilotage des applications ?',NULL),(14,2,3,'Qui a écrit l\'article \"Aide à l’analyse des parcours d’apprentissage en IUT par reconnaissance de procédés et recommandations à base de traces\" ?',NULL),(15,3,3,'Qui travaille sur l\'adaptation dynamique des contenus, notamment par scénarisation ?',NULL),(16,2,3,'Qui a écrit l\'article \"Scénarisation et exécution adaptative\" ?',NULL),(17,3,2,'Qui travaille sur les problématiques et techniques liées à l\'indexation ?',NULL),(18,1,1,'Qui était le directeur de thèse de Mickael Coustaty ?',NULL),(19,1,1,'Qui était le directeur de thèse de Frédéric Bertrand ?',NULL),(20,1,1,'Quels chercheurs font partie de l’équipe de projet eBDthèque ?',NULL),(21,1,2,'Quels chercheurs font partie de l’équipe de projet SHADES ?',NULL),(22,1,2,'Quels chercheurs font partie de l’équipe de projet Chaire d’excellence ‘Gestion du conflit et de l’après-conflit’?',NULL),(23,2,1,'Qui a écrit l’article ‘Generation algorithm of a concept lattice with limited object access’ ?',NULL),(24,2,1,'Qui a écrit l’article ‘Vers une réduction du fossé sémantique dans le traitement des images de documents anciens à base d’ontologies : Application aux lettrines’ ?',NULL),(25,2,2,'Qui a écrit l’article ‘Real-time face tracking for attention aware adaptive games’ ?',NULL),(26,2,2,'Qui a écrit l’article ‘A new shape descriptor working in Discrete Cosine space : application to graphical symbols recognition’ ?',NULL),(27,3,2,'Qui travaille les extractions et les manipulations de connaissances ?',NULL),(28,3,3,'Qui travaille sur la modélisation d’expressions temporelles exprimant une périodicité ?',NULL),(29,3,3,'Qui travaille sur le pilotage de récits interactifs ?',NULL),(30,3,3,'Qui travaille sur la mobilité et l’intégration des données ?',NULL),(31,4,2,NULL,'img/category4/DSC_0514.JPG'),(32,4,2,NULL,'img/category4/DSC_0515.JPG'),(33,4,3,NULL,'img/category4/DSC_0516.JPG'),(34,4,1,NULL,'img/category4/DSC_0517.JPG'),(35,4,2,NULL,'img/category4/DSC_0518.JPG'),(36,4,2,NULL,'img/category4/DSC_0519.JPG'),(37,4,2,NULL,'img/category4/DSC_0520.JPG'),(38,4,1,NULL,'img/category4/DSC_0521.JPG'),(39,4,2,NULL,'img/category4/DSC_0522.JPG'),(40,4,3,NULL,'img/category4/DSC_0523.JPG'),(41,4,3,NULL,'img/category4/DSC_0524.JPG'),(42,4,2,NULL,'img/category4/DSC_0525.JPG'),(43,4,1,NULL,'img/category4/DSC_0526.JPG'),(44,4,2,NULL,'img/category4/DSC_0527.JPG'),(46,4,1,NULL,'img/category4/DSC_0529.JPG'),(47,4,3,NULL,'img/category4/DSC_0530.JPG');
 /*!40000 ALTER TABLE `question` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -181,7 +181,7 @@ CREATE TABLE `question_reponse` (
 
 LOCK TABLES `question_reponse` WRITE;
 /*!40000 ALTER TABLE `question_reponse` DISABLE KEYS */;
-INSERT INTO `question_reponse` VALUES (2,2),(9,2),(20,2),(23,2),(24,2),(28,3),(5,4),(24,4),(30,4),(20,5),(21,5),(14,6),(16,6),(29,6),(22,7),(25,7),(7,8),(21,8),(24,8),(9,9),(23,9),(27,9),(16,11),(25,11),(26,13),(21,16),(22,16),(19,19),(24,19),(1,25),(8,25),(26,25),(7,26),(16,26),(18,26),(20,26),(21,26),(22,26),(26,26),(3,27),(6,27),(15,27),(16,27),(25,27),(13,28),(14,28),(3,30),(4,30),(20,30),(11,32),(12,32),(10,33),(7,34),(8,34);
+INSERT INTO `question_reponse` VALUES (2,2),(9,2),(20,2),(23,2),(24,2),(34,2),(35,2),(36,2),(28,3),(5,4),(24,4),(30,4),(39,4),(20,5),(21,5),(33,5),(35,5),(36,5),(37,5),(40,5),(44,5),(14,6),(16,6),(29,6),(22,7),(25,7),(36,7),(7,8),(21,8),(24,8),(41,8),(42,8),(47,8),(9,9),(23,9),(27,9),(16,11),(25,11),(46,12),(26,13),(21,16),(22,16),(38,16),(41,16),(43,16),(47,17),(19,19),(24,19),(43,22),(1,25),(8,25),(26,25),(7,26),(16,26),(18,26),(20,26),(21,26),(22,26),(26,26),(33,26),(35,26),(36,26),(37,26),(38,26),(40,26),(41,26),(42,26),(3,27),(6,27),(15,27),(16,27),(25,27),(46,27),(13,28),(14,28),(3,30),(4,30),(20,30),(34,30),(35,30),(46,30),(11,32),(12,32),(10,33),(7,34),(8,34),(32,34),(46,38),(40,90),(44,92),(47,107),(42,125),(31,127),(33,127),(35,127);
 /*!40000 ALTER TABLE `question_reponse` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -245,4 +245,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-10-10 18:59:59
+-- Dump completed on 2019-10-26 15:09:00
